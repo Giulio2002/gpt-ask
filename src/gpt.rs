cdd192c312fffc092f97bc2b0905a1c1e08611ea
Giulio Rebuffo <giulio.rebuffo@gmail.com>
2023-01-05 01:26:51 +0100

    added comments

--- src/gpt.rs
+++ src/gpt.rs
@@ -1,5 +1,3 @@
-
-
 extern crate reqwest;
 extern crate serde;
 
@@ -11,9 +9,14 @@ use crate::{
     },
 };
 
+// Constants for default API URL and a specific model name
 pub const API_URL_DEFAULT: &str = "https://api.openai.com/v1";
 pub const DA_VINCI_MODEL: &str = "text-davinci-003";
+
+// Maximum number of tokens that the API should return in its response
 pub const MAX_TOKENS: usize = 4000;
+
+// Struct for holding client configuration and state
 pub struct OpenAIClient {
     api_key: String,
     api_url: String,
@@ -22,18 +25,27 @@ pub struct OpenAIClient {
 }
 
 impl OpenAIClient {
+    // Constructor for creating a new OpenAIClient
     pub fn new(api_key: String, api_url: String, max_tokens: usize, model: String) -> OpenAIClient {
         OpenAIClient {api_key, api_url, max_tokens, model}
     }
 
-    pub async fn ask(self: &Self, prompt: String) -> Result<String, Error> {    
+    // Asynchronously send a completion request to the API and return the response
+    pub async fn ask(self: &Self, prompt: String) -> Result<String, Error> {
+        // Create a new HTTP client
         let client = reqwest::Client::new();
+
+        // Construct the completion request
         let request: CompletionRequest = CompletionRequest {
             model: self.model.to_owned(),
             max_tokens: self.max_tokens,
             prompt,
         };
+
+        // Construct the endpoint URL
         let endpoint = format!("{}/completions", self.api_url);
+
+        // Send the request and store the response
         let res = client
             .post(endpoint)
             .json(&request)
@@ -41,9 +53,13 @@ impl OpenAIClient {
             .send()
             .await?;
 
+        // Get the response body as a string
         let text = res.text().await?;
+
+        // Deserialize the response JSON
         let response: CompletionResponse = serde_json::from_str(&text)?;
+
+        // Return the text of the first choice in the response
         Ok(response.choices[0].text.to_owned())
     }
 }
-
